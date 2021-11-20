27157abd75f2c1a3209392f9497b8e2e0b5b3a22
Mr.Python_34 <74334430+jciafardo@users.noreply.github.com>
2021-11-19 21:21:14 -0500

    Add files via upload

---
+++ Setup.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+path = ("C:\Hacks\python\programs\WebScraping\instagram_automation\chromedriver")
+driver = webdriver.Chrome(path)
+driver.get("https://www.instagram.com/")
