1eba12a5e9202cd2a11cd9cbe76f2fd53936bf8b
Mr.Python_34 <74334430+jciafardo@users.noreply.github.com>
2021-12-10 14:19:37 -0500

    Update like_pics.py

--- like_pics.py
+++ like_pics.py
@@ -2,7 +2,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
-from WebScraping.instagram_auto_liking_tool import Setup
+import Setup
 
 driver = Setup.driver
 
